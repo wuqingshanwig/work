3261fffe3bd2c247f1dd2c6ee872f43263247135
wuqingshanwig <1921890771@qq.com>
2019-12-17 19:38:24 +0800

    up

--- src/wig/CountChar.java
+++ src/wig/CountChar.java
@@ -3,33 +3,29 @@ package wig;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 判断一个字符文件中指定的字符串出现的次数,以及出现的具体位置(第几行第几个字符)
  */
 public class CountChar {
-    private static int count =0;//字符在字符串中出现的次数.
-    private static Map<Integer,Integer> charLocal = new HashMap<Integer,Integer>();//行和具体位置的映射
-
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException{
+        String str = "儿子";
         FileReader fr = new FileReader("Test");
         BufferedReader br = new BufferedReader(fr);
-       String str = null;
-        while((str = br.readLine()) != null){
-          counts(str);
+        String line = null;
+        int lineCount = 0;
+        int count = 0;
+        while((line = br.readLine()) != null){
+            lineCount++;
+            int index = -str.length();
+            while ((index = line.indexOf(str,index+str.length()))>-1){
+                System.out.println("该字符串出现在"+lineCount+"行"+index+"位置");
+                count++;
+            }
 
         }
-    }
-    public static int  counts(String str){
-        while (str.contains("我的")){
-            count++;
-        }
-        return count;
-    }
-    public static Map find (String str){
-
-        return null;
+       br.close();
+        fr.close();
+        System.out.println("出现" + count +"次");
     }
 }
